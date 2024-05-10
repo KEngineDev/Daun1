8b56d76d496ccbd559e00b4291bd016acb77c109
KEngineDev <155124426+KEngineDev@users.noreply.github.com>
2024-05-10 08:25:48 -0700

    1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
-const gameName = "Daun1";
+const gameName = "TapTap";
 const queries = {};
 server.use(express.static(path.join(__dirname, 'Daun1')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
